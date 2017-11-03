ad03f3e3591d07ad082eb58f5487e3b46611ce56
dical <dcallejastar@gmail.com>
2017-11-03 00:03:39 -0300

    nuevo diseño sin terminar

--- src/components/formCategories.js
+++ src/components/formCategories.js
@@ -3,10 +3,75 @@ import React, { Component } from 'react';
 import Button from 'material-ui/Button';
 
 class Categories extends Component {
+    componentWillMount() {
+        if (document.getElementById('title').innerText === 'Recientes') {
+            this.handleRecent()
+        } else {
+            this.handleParticipants()
+        }
+    }
+
+    state = {
+        recent: {
+            color: 'primary'
+        },
+        participants: {
+            color: 'default'
+        }
+    };
+
+    handleRecent = () => {
+        document.getElementById('title').innerText = 'Recientes';
+
+        this.props.request.sort = 'recent';
+
+        this.setState({
+            recent: {
+                color: 'primary'
+            },
+            participants: {
+                color: 'default'
+            }
+        }, this.props.onChange )
+    };
+
+    handleParticipants = () => {
+        document.getElementById('title').innerText = 'Mas participantes';
+
+        this.props.request.sort = 'participants';
+
+        this.setState({
+            recent: {
+                color: 'default'
+            },
+            participants: {
+                color: 'primary'
+            }
+        }, this.props.onChange )
+    };
+
     render() {
-        return <form>
-            <Button>Recientes</Button>
-            <Button>Con mas participantes</Button>
+        return <form
+            style={{ display: 'flex' }}
+        >
+            <Button
+                selected
+                raised
+                color={ this.state.recent.color }
+                style={{ width: '50%', marginRight: 8 }}
+                onClick={ this.handleRecent }
+            >
+                Recientes
+            </Button>
+
+            <Button
+                raised
+                color={ this.state.participants.color }
+                style={{ width: '50%', marginLeft: 8 }}
+                onClick={ this.handleParticipants }
+            >
+                Con mas participantes
+            </Button>
         </form>
     }
 }
--- src/components/listActivity.js
+++ src/components/listActivity.js
@@ -7,6 +7,7 @@ import Dialog from 'material-ui/Dialog';
 import Icon from 'material-ui/Icon';
 import List, { ListItem, ListItemText } from 'material-ui/List';
 import Slide from 'material-ui/transitions/Slide';
+import TextField from 'material-ui/TextField';
 import Typography from 'material-ui/Typography';
 
 import Categories from './formCategories'
@@ -17,14 +18,17 @@ class Activities extends Component {
         dialogs: {
             tags: false,
             filters: false
+        },
+        request: {
+            q: '',
+            sort: 'recent',
+            tags: []
         }
     };
 
     componentDidMount() {
         document.getElementById('title').innerText = 'Recientes';
-
         document.getElementById('header').classList.remove('transparent');
-        
         document.getElementById('shell').style.padding = '64px 0';
 
         ['title', 'search', 'filter', 'down', 'bottom-navigation'].forEach(function(id) {
@@ -39,9 +43,19 @@ class Activities extends Component {
             document.getElementById('add').style.display = 'none';
         }
 
-        let request = new XMLHttpRequest(), onUpdate = this.handleUpdateData;
+        this.handleRequest()
+    };
 
-        request.open('GET', 'http://' + window.location.hostname + ':8081/events', true);
+    handleRequest = () => {
+        let request = new XMLHttpRequest(),
+            onUpdate = this.handleUpdateData,
+            query = this.state.request.q !== '' ? 'q=' + this.state.request.q + '&' : '';
+
+        query = this.state.request.sort !== '' ? query + 'sort=' + this.state.request.sort + '&' : query + '';
+        query = this.state.request.tags.length > 0 ? query + 'tags=' + this.state.request.tags.join(',') : query + '';
+        query = query !== '' ? '?' + query : query;
+
+        request.open('GET', 'http://' + window.location.hostname + ':8081/events' + query, true);
 
         request.onreadystatechange = function() {
             if (request.readyState === 4) {
@@ -56,6 +70,16 @@ class Activities extends Component {
         };
 
         request.send()
+    };
+
+    handleSearch = (event) => {
+        this.setState({
+            request: {
+                q: event.target.value,
+                sort: this.state.request.sort,
+                tags: this.state.request.tags
+            }
+        }, this.handleRequest )
     };
 
     handleUpdateData = (data) => {
@@ -75,6 +99,9 @@ class Activities extends Component {
     };
 
     handleDialogFilters = () => {
+        document.getElementById('down').style.display = document.getElementById('down').style.display === 'none' ? '' : 'none';
+        document.getElementById('search').style.display = document.getElementById('search').style.display === 'none' ? '' : 'none';
+
         this.setState({
             dialogs: {
                 tags: this.state.dialogs.tags,
@@ -93,7 +120,14 @@ class Activities extends Component {
                         display: 'none'
                     }}
                 >
-                    Buscar
+                    <TextField
+                        id="search-name"
+                        autoComplete="off"
+                        fullWidth
+                        label="Buscar..."
+                        onChange={ this.handleSearch }
+                        type="text"
+                    />
                 </div>
 
                 <List>
@@ -135,7 +169,7 @@ class Activities extends Component {
                     <div
                         style={{ padding: '32px 16px' }}
                     >
-                        <Categories/>
+                        <Categories request={ this.state.request } onChange={ this.handleRequest }/>
                     </div>
                 </Dialog>
 
